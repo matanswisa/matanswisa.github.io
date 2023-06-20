610137ae39a7a6ee1d19a29d73d700b05011dd3b
mynameismatan <matanamar165@gmail.com>
2023-06-20 10:30:02 +0300

    Create button in daily stats page

--- src/components/DailyStatsCard/DailyStatsCard.js
+++ src/components/DailyStatsCard/DailyStatsCard.js
@@ -1,11 +1,32 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Colors from '../color-utils/Colors'
 
 
+import {
+
+    Box,
+    Grid,
+    Card,
+    Table,
+    Stack,
+    Paper,
+    Avatar,
+    Button,
+    Popover,
+    Checkbox,
+    TableRow,
+    MenuItem,
+    TableBody,
+    TableCell,
+    Container,
+    Typography,
+    IconButton,
+    TableContainer,
+    TablePagination,
+  } from '@mui/material';
+  
+
+import Iconify from '../iconify/Iconify';
 
 import './DailyStatsCard.css';
 
@@ -18,41 +39,56 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 
-const numbers = ["Total Trades : 2 " , "Winners : 2", "Profit Factor : 1RR","Winrate : 100.00%", "Losers : 0", "Commissions :13$"];
+const numbers = ["Total Trades : " , "Winners : ", "Profit Factor :","Winrate : ", "Losers : ", "Commissions :"];
 
 export default function DailyStatsCard() {
   return (
-    <Box sx={{ flexGrow: 1  , p: 3, border: '2px ridge ', boxShadow:'5px 10px 30px #888888'}}>
-      <Grid container spacing={2}>
+    <Box sx={{ flexGrow: 2  , p: 3, border: '2px ridge ', boxShadow:'5px 10px 30px #888888'}}>
+        
+      <Grid container spacing={1}>
         <Grid item xs={8}>
 
 
         
 
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-            <Grid item xs={4}>
+        <Grid container rowSpacing={3} columnSpacing={{ xs: 2, sm: 2, md: 1 }}>
+            <Grid item xs={5}>
                 <h2 className="date">Wed, Jul 14 2022</h2>
             </Grid>
            
-            <Grid item xs={4}>
+            <Grid item xs={5}>
                 <h2 className="net">Net P&L $1259.46</h2>
+                
             </Grid>
+
+            
+             
+           
+         
             </Grid>
             </Grid>
 
+        
         
        
-        <Grid container spacing={{ xs: 2, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {Array.from(Array(12)).map((_, index) => (
-          <Grid  item xs={3} sm={4} md={4} key={index}>
-             {numbers[index] } 
+        <Grid container spacing={{ xs: 3, md: 4 }} columns={{ xs: 2, sm: 11, md: 12 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid  item xs={2} sm={4} md={4} key={index}>
+            {numbers[index] } <h4>values</h4>
           </Grid>
+      
         ))}
-
+            
         
       </Grid>
 
       </Grid>
+      <Grid item xs={2}>
+            <Button variant="contained">Edit Note</Button>
+
+                
+            </Grid>
+
     </Box>
   );
 }
