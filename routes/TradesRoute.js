3733c71540572a67eec4a96703e355cbfb3e908d
mynameismatan <matanamar165@gmail.com>
2023-06-25 23:31:08 +0300

    edit mode update

--- routes/TradesRoute.js
+++ routes/TradesRoute.js
@@ -19,6 +19,19 @@ router.post("/addTrade", async (req, res) => {
 });
 
 
+router.post("/editTrade", async (req, res) => {
+    try {
+        const data = req.body;
+        const result = await Trade.findByIdAndUpdate(tradeId,data);
+        res.status(200).send(`Trade ${result._id} added succefully`);
+
+    } catch (err) {
+        console.err(err);
+        res.status(500).send('Error when adding a trade');
+    }
+});
+
+
 router.get('/fetchTrades', async (req, res) => {
     try {
         const trades = await Trade.find({});
