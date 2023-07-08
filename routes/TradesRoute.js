f3d7a843c51f93f056c972400ef8e88b3c0c5e68
mynameismatan <matanamar165@gmail.com>
2023-07-08 20:29:23 +0300

    ftr upload image

--- routes/TradesRoute.js
+++ routes/TradesRoute.js
@@ -1,7 +1,29 @@
 import { Router } from "express";
 import Trade from "../models/trade.js";
-
+import multer from 'multer';
+import Path from 'path';
 const router = Router();
+
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const currentFilePath = fileURLToPath(import.meta.url);
+const currentDirPath = dirname(currentFilePath);
+const uploadsDirPath = Path.join(currentDirPath, '..', '/uploads/');
+
+// Multer configuration:
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadsDirPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
 
 /**
  * This is a test
@@ -10,10 +32,10 @@ router.post("/addTrade", async (req, res) => {
   try {
     const data = req.body;
     const result = await Trade.create(data);
-    res.status(200).send(`Trade ${result._id} added succefully`);
+    res.status(200).json({ tradeId: result._id });
 
   } catch (err) {
-    console.err(err);
+    console.error(err);
     res.status(500).send('Error when adding a trade');
   }
 });
@@ -23,9 +45,8 @@ router.post("/editTrade", async (req, res) => {
   try {
     const data = req.body;
     console.log(data);
-    const result = await Trade.findOneAndUpdate({ _id: data.tradeId }, data);
-    console.log(result);
-    res.status(200).send(`Trade ${result._id} has beed edited succefully`);
+    const result = await Trade.findByIdAndUpdate(data.tradeId, { data });
+    res.status(200).send(`Trade ${result._id} added succefully`);
 
   } catch (err) {
     console.err(err);
@@ -130,28 +151,33 @@ router.get('/ShowInfoByDates', async (req, res) => {
   }
 });
 
-console.log("tesdt");
-
-  router.get('/ShowNumOfTradeTotalPnlInfoByDates', async (req, res) => {
-    try {
-      const tradesByDate = await Trade.aggregate([
-        {
-          $group: {
-            _id: { $dateToString: { format: '%Y-%m-%d', date: '$entryDate' } },
-            trades: { $sum: 1 }, // Count of trades
-            totalPnL: { $sum: '$netPnL' },
-          },
-        },
-        { $sort: { _id: -1 } }, // Sort by descending entryDate
-      ]);
-  
-      res.json(tradesByDate);
-      console.log(tradesByDate);
-    } catch (error) {
-      console.error('Error fetching trades:', error);
-      res.status(500).json({ error: 'An error occurred' });
-    }
-  });
 
 
-export default router;
+router.post('/uploadTradeImage', upload.single('file'), async (req, res) => {
+  console.log(req.body)
+  try {
+    const { tradeId } = req.body;
+    const { path, originalname } = req.file;
+
+    // Handle the uploaded image as needed
+    // For example, you can save the image path or perform image processing
+
+    // Update the Trade document with the image details
+    const imagePath = Path.join(uploadsDirPath, originalname)
+    const trade = await Trade.findByIdAndUpdate(tradeId, {
+      $set: {
+        image: imagePath
+      }
+    });
+    if (trade) { res.status(200).json({ message: 'Image uploaded successfully' }); return; }
+    res.status(400).send("Can't save trading image");
+
+  } catch (err) {
+    console.error('Error uploading trade image:', err);
+    res.status(500).json({ error: 'An error occurred while uploading the image' });
+  }
+});
+
+
+
+export default router;
