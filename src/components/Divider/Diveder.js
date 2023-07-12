53e332621c04e791666be4590a217c7fef553c2a
mynameismatan <matanamar165@gmail.com>
2023-07-13 01:10:55 +0300

    update daliy stat page

--- src/components/Divider/Diveder.js
+++ src/components/Divider/Diveder.js
@@ -16,6 +16,15 @@ import Paper from '@mui/material/Paper';
 
 
 
+const ProfitFactor = (trade) => {
+  
+
+  if (trade.totalLoss == 0 || trade.totalWin == 0) return 0;
+
+  return (trade.totalWin / trade.totalLoss < 0 ? trade.totalWin / trade.totalLoss * -1 : trade.totalWin / trade.totalLoss).toFixed(2);
+}
+
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -39,79 +48,76 @@ function getFormattedDate(dateString) {
 
   return `${abbreviatedWeekday}, ${abbreviatedMonth} ${day} ${year}`;
 }
-export default function Diveder(props) {
-    const totalPnL = props.trade.totalPnL;
-    const isNegative = totalPnL < 0;
-    const winRate = (props.trade.win / (props.trade.win + props.trade.loss) * 100).toFixed(2); // Calculate win rate percentage
-    console.log(props.trade);
-    return (
-
-      <Box sx={{ width: '1400px', maxWidth: 1200, bgcolor: 'background.paper' }}>
-        <Box sx={{ my: 8, mx: 2 }}>
-       
-          <Grid container rowSpacing={3} alignItems="center">
-                  <Grid item xs>
-                  <Typography gutterBottom variant="h4" component="div">
-  {getFormattedDate(props.trade._id)} 
-</Typography>
-                  
-                  </Grid>
-               
-     
-                  <Grid item>
-                    <Typography
-                      gutterBottom
-                      variant="h4"
-                      component="div"
-                      sx={{ color: isNegative ? Colors.red : Colors.green }}
-                    >
-                      Net P&L ${props.trade.Commission <0 ? totalPnL + props.trade.Commission : totalPnL}
-                      
-                    </Typography>
-                  </Grid>
-          </Grid>
-          <Typography color="text.primary" variant="body1">
-            {props.trade.numberOfTrades} Trades
-          </Typography>
-        </Box>
-   
-        
-        <Grid container rowSpacing={3} columnSpacing={{ xs: 3, sm: 2, md: 5 }}>
-               <Grid item xs={6}>
-                 <Item><Typography color="text.primary" variant="body1"> Winners&nbsp;&nbsp;&nbsp;&nbsp; {props.trade.win}</Typography> </Item>
-              </Grid>
-
-                      
-               <Grid item xs={6}>
-                  <Item> <Typography color="text.primary" variant="body1"> Win rate &nbsp;&nbsp;&nbsp;&nbsp;{winRate} % </Typography>   </Item>
-               </Grid>
-        </Grid>
-
-      
-        <Grid container rowSpacing={12} columnSpacing={{ xs: 13, sm: 5, md: 5 }}>
-      
-            <Grid item xs={6}>
-                        <Item>  <Typography color="text.primary" variant="body1"> Losers  &nbsp;&nbsp;&nbsp;&nbsp;{props.trade.loss} </Typography></Item>
-            </Grid>
-
-
-             <Grid item xs={6}>
-
-                        <Item> <Typography color="text.primary" variant="body1" >Gross P&L&nbsp;&nbsp;&nbsp;&nbsp;  ${totalPnL} </Typography></Item>
-              </Grid>
-            
-        </Grid>
-
-
   
-        
-    
-        <Divider variant="middle"  style={{ background: 'grey' }}   spacing={25}/>
-        <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
-          <Button>Edit note</Button>
+
+export default function Diveder(props) {
+  const totalPnL = props.trade.totalPnL;
+  const isNegative = totalPnL < 0;
+  const winRate = ((props.trade.win / (props.trade.win + props.trade.loss)) * 100).toFixed(2);
+
+  return (
+    <Box sx={{ width: '1400px', maxWidth: 1200, bgcolor: 'background.paper' }}>
+      <Box sx={{ my: 8, mx: 2 }}>
+        <Grid container rowSpacing={3} alignItems="center">
+          <Grid item xs={6}>
+            <Typography gutterBottom variant="h4" component="div">
+              {getFormattedDate(props.trade._id)}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography gutterBottom variant="h4" component="div" sx={{ color: isNegative ? Colors.red : Colors.green }}>
+              Net P&L ${props.trade.Commission < 0 ? totalPnL + props.trade.Commission : totalPnL}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Typography color="text.primary" variant="body1">
+          {props.trade.numberOfTrades} Trades
+        </Typography>
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Item>
+            <Typography color="text.primary" variant="body1">
+              Winners&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>{props.trade.win}</b>
+            </Typography>
+          </Item>
+          <Item>
+            <Typography color="text.primary" variant="body1">
+              Losers&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>{props.trade.loss}</b>
+            </Typography>
+          </Item>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', marginLeft: '60px', marginRight: '60px' }}>
+          <Item>
+            <Typography color="text.primary" variant="body1">
+              Win rate&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>{winRate} %</b>
+            </Typography>
+          </Item>
+          <Item>
+            <Typography color="text.primary" variant="body1">
+              Gross P&L&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>${totalPnL}</b>
+            </Typography>
+          </Item>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Item>
+            <Typography color="text.primary" variant="body1">
+              Commission&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>${props.trade.Commission}</b>
+            </Typography>
+          </Item>
+          <Item>
+            <Typography color="text.primary" variant="body1">
+              Profit Factor&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>{ProfitFactor(props.trade)}</b>
+            </Typography>
+          </Item>
         </Box>
       </Box>
-      
-    );
-  }
 
+      <Divider variant="middle" style={{ background: 'grey' }} spacing={25} />
+      <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
+        <Button>Edit note</Button>
+      </Box>
+    </Box>
+  );
+}
