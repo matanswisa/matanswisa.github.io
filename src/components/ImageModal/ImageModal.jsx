9cbd1c39c7f0410ddbabc777f7d983ba2d978c24
matan swisa <matanswisa01@gmail.com>
2023-07-13 16:05:54 +0300

    change display of image , add changes to dailyStats

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -12,11 +12,60 @@ const ImageDialog = ({ open, handleClose, imageData, tradeComments }) => {
         }
     }, [imageData]);
 
+    const openImageWindow = () => {
+        const imageWindow = window.open('', '_blank', 'fullscreen=yes');
+        const imageDocument = imageWindow.document;
+        imageDocument.write(`
+      <html>
+        <head>
+          <title>Fullscreen Image</title>
+          <style>
+            body {
+              margin: 0;
+              padding: 0;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 100vh;
+              background-color: #000;
+            }
+            img {
+              max-width: 100%;
+              max-height: 100%;
+              cursor: pointer;
+            }
+            .close-icon {
+              position: fixed;
+              top: 10px;
+              right: 10px;
+              color: #fff;
+              cursor: pointer;
+              font-size: 24px;
+              padding: 8px;
+              background-color: rgba(0, 0, 0, 0.6);
+              border-radius: 50%;
+            }
+          </style>
+        </head>
+        <body>
+          <span class="close-icon" onclick="window.close()">✕</span>
+          <img src="${imageSrc}" alt="Trade Image">
+        </body>
+      </html>
+    `);
+        imageDocument.close();
+    };
+
     return (
         <Dialog open={open} onClose={handleClose} maxWidth="md">
             <DialogTitle>Trade details</DialogTitle>
             <DialogContent>
-                <img src={imageSrc} alt="Trade Image" style={{ maxWidth: '100%', maxHeight: '80vh' }} />
+                <img
+                    src={imageSrc}
+                    alt="Trade Image"
+                    style={{ cursor: 'pointer', maxWidth: '100%', maxHeight: '80vh' }}
+                    onClick={openImageWindow}
+                />
                 <h4>My comments:</h4>
                 <br />
                 <p>{tradeComments}</p>
--- src/pages/DailyStatsPage.js
+++ src/pages/DailyStatsPage.js
@@ -1,10 +1,11 @@
+import Divider from '../components/Divider/Diveder';
+
 import { Helmet } from 'react-helmet-async';
-import { filter } from 'lodash';
+import { filter, includes, lowerCase } from 'lodash';
 import { sentenceCase } from 'change-case';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTrades } from '../redux-toolkit/tradesSlice';
-
 
 // @mui
 import {
@@ -25,56 +26,80 @@ import {
   IconButton,
   TableContainer,
   TablePagination,
-  DataGrid
+  DataGrid,
+  TextField,
+  Select,
+  FormControl,
+  InputLabel,
 } from '@mui/material';
 
-import Diveder from '../components/Divider/Diveder';
+
 // components
 
 import Iconify from '../components/iconify';
 import api from '../api/api';
 
-
 export default function DailyStatsPage() {
-
   const [trades, setTrades] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [searchOption, setSearchOption] = useState('name');
+  const [filteredTrades, setFilteredTrades] = useState([]);
+
   useEffect(() => {
-    api.get("/api/DailyStatsInfo").then(
-      (res)=>{setTrades(res.data)   
-     
-      
-     
-        }
-    ).catch()
-  },[])
-  
+    api.get('/api/DailyStatsInfo')
+      .then((res) => {
+        setTrades(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const filteredData = filter(trades, (trade) => {
+      const tradeValue = trade[searchOption]?.toString().toLowerCase() ?? '';
+
+      return includes(tradeValue, searchValue.toLowerCase());
+    });
+
+    setFilteredTrades(filteredData);
+  }, [searchValue, searchOption, trades]);
 
   return (
     <>
       <Helmet>
-        <title> Daily Stats </title>
+        <title>Daily Stats</title>
       </Helmet>
-
+      <FormControl variant="outlined" style={{ minWidth: 120 }}>
+        <InputLabel>Search By</InputLabel>
+        <Select
+          value={searchOption}
+          onChange={(e) => setSearchOption(e.target.value)}
+          label="Search By"
+        >
+          <MenuItem value="name">Name</MenuItem>
+          <MenuItem value="numberOfTrades">Number of Trades</MenuItem>
+          <MenuItem value="date">Date</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField
+        label="Search"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
       <Container>
+        <div style={{ maxHeight: '700px', overflowY: 'scroll' }}>
+          <Stack direction="row" alignItems="center" justifyContent="space-between" mb={8}>
+            <Typography variant="h4" gutterBottom>
+              Daily Stats
+            </Typography>
 
-      <div style={{ maxHeight: '700px', overflowY: 'scroll' }}>
-   
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={8}>
-          <Typography variant="h4" gutterBottom>
-           
-            {trades !== undefined && trades.map((trade) => {
-              return <Diveder trade={trade} />
-            })}
+          </Stack>
 
-
-            Daily Stats
-          </Typography>
-
-
-        </Stack>
-           
-    </div>
-
+          {filteredTrades.map((trade) => (
+            <Divider trade={trade} key={trade.id} />
+          ))}
+        </div>
       </Container>
     </>
   );
