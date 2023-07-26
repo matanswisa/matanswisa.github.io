d6fac983c2ca4867c847c2e36e657463a1035fb3
mynameismatan <matanamar165@gmail.com>
2023-07-26 18:30:23 +0300

    .

--- src/components/appBar/appBar.js
+++ src/components/appBar/appBar.js
@@ -7,13 +7,42 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SelectAccount from '../../components/accounts/selectAccount'
+import api from '../../api/api';
+import { useEffect, useState } from 'react';
 export default function ButtonAppBar() {
+  const [accounts, setAccounts] = useState([]);
+
+
+
+
+  const fetchAccounts = async () => {
+    try {
+      const response = await api.get('/api/accounts');
+      setAccounts(response.data);
+
+      
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  
+  useEffect(() => {
+    fetchAccounts();
+    
+  }, []);
+
+
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
        <AppBar position="static" sx={{ backgroundColor: '#ffffff', boxShadow: 'none',  }}>
        <Toolbar sx={{ display: 'flex', justifyContent: 'flex-end'  }}>
-         
-        <SelectAccount/>
+
+
+       {accounts.length > 0 && <SelectAccount />}
+     
           
         </Toolbar>
       </AppBar>
