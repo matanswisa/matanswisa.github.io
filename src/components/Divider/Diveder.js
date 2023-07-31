a1ce8d4c93cbd119a01285e53cb86dd518c55e6b
Matan Amar <matan@skysoftsl.com>
2023-07-31 16:24:27 +0300

    ..

--- src/components/Divider/Diveder.js
+++ src/components/Divider/Diveder.js
@@ -75,7 +75,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 1500,
+  width: 1200,
   height :560,
   bgcolor: 'background.paper',
   border: '2px solid #000',
