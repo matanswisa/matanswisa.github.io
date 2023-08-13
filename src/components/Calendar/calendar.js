c1a6a02ab5f00831b8350796736b1a4c268eb9d9
Matan Swisa <matanswisa01@gmail.com>
2023-08-13 21:01:35 +0300

    change currentAccount.trades

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -1,11 +1,11 @@
 
 
 import { Calendar } from 'rsuite';
-import { useState,  useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './styles.css';
 import './calendar.css';
 import api from '../../api/api';
-import userSlice, { selectCurrentAccount , selectUser} from '../../redux-toolkit/userSlice';
+import userSlice, { selectCurrentAccount, selectUser } from '../../redux-toolkit/userSlice';
 import { configAuth } from '../../api/configAuth';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -16,13 +16,13 @@ function getTodoList(info, date) {
     return itemDate.getDate() === date.getDate(); // Check if the item date matches the provided date
   });
 
- 
+
   return filteredInfo.map((item) => ({
     numoftrades: item.trades,
     title: item.trades > 1 ? 'trades' : 'trade',
     amount: item.totalPnL,
   }));
-  
+
 }
 
 const CalendarComponent = () => {
@@ -32,15 +32,14 @@ const CalendarComponent = () => {
 
 
   useEffect(() => {
-    api.post('/api/ShowNumOfTradeTotalPnlInfoByDates',{trades: currentAccount.trades},configAuth)
-      .then((res) => {
-        setCalendarTrades(res.data);
-
-
-     
-      })
-      .catch();
-  }, [currentAccount.trades]);
+    if (currentAccount?.trades && currentAccount.trades.length) {
+      api.post('/api/ShowNumOfTradeTotalPnlInfoByDates', { trades: currentAccount.trades }, configAuth)
+        .then((res) => {
+          setCalendarTrades(res.data);
+        })
+        .catch();
+    }
+  }, []);
 
 
 
@@ -50,22 +49,22 @@ const CalendarComponent = () => {
   function renderCell(date) {
     const list = getTodoList(calendarTrades, date);
     const displayList = list.filter((item, index) => index < 2);
-  
+
     const desiredDays = calendarTrades
       .filter((trade) => {
         const tradeDate = new Date(trade._id); // Convert the _id to a Date object
         const tradeMonth = tradeDate.getMonth();
         const currentDate = new Date();
         const currentMonth = currentDate.getMonth();
-  
+
         // Filter trades from the current month and the previous month
         return tradeMonth === date.getMonth() || tradeMonth === currentMonth - 1;
       })
       .map((trade) => parseInt(trade._id.split("-")[2] > 0 ? trade._id.split("-")[2] : trade._id.split("-")[2] % 10));
-  
+
     const isDesiredDay = desiredDays.includes(date.getDate());
     const isCurrentMonth = date.getMonth() === new Date().getMonth();
-  
+
     if (isDesiredDay && isCurrentMonth && list.length) {
       return (
         <div>
@@ -87,7 +86,7 @@ const CalendarComponent = () => {
         </div>
       );
     }
-  
+
     return null;
   }
   return <Calendar bordered renderCell={renderCell} />;
