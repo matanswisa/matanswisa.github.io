5aa96eb5f4614e76e2da2e0fde766fc2ad8c0575
matan swisa <matanswisa01@gmail.com>
2023-08-21 10:46:40 +0300

    add logger for csv trades binance

--- utils/csvTradesFileImportsBinance.js
+++ utils/csvTradesFileImportsBinance.js
@@ -9,43 +9,43 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 function groupByPositionId(array) {
-    const grouped = {};
+  const grouped = {};
 
-    array.forEach(item => {
-        const positionId = item['Position ID'];
+  array.forEach(item => {
+    const positionId = item['Position ID'];
 
-        if (!grouped[positionId]) {
-            grouped[positionId] = [];
-        }
+    if (!grouped[positionId]) {
+      grouped[positionId] = [];
+    }
 
-        grouped[positionId].push(item);
-    });
+    grouped[positionId].push(item);
+  });
 
-    return grouped;
+  return grouped;
 }
 
 
 function handleMergeRows(excelJsonData) {
-    const mergedRows = [];
-    const groupedRows = {};
+  const mergedRows = [];
+  const groupedRows = {};
 
-    excelJsonData.forEach(row => {
-        const key = `${row.Symbol}-${row.Side}-${row.Price}`;
-        if (!groupedRows[key]) {
-            groupedRows[key] = { ...row };
-        } else {
-            groupedRows[key].Quantity = (+groupedRows[key].Quantity + +row.Quantity).toFixed(8);
-            groupedRows[key].Amount = (+groupedRows[key].Amount + +row.Amount).toFixed(16);
-            groupedRows[key].Fee = (+groupedRows[key].Fee + +row.Fee).toFixed(8);
-            groupedRows[key]['Realized Profit'] = (+groupedRows[key]['Realized Profit'] + +row['Realized Profit']).toFixed(8);
-        }
-    });
-
-    for (const key in groupedRows) {
-        mergedRows.push(groupedRows[key]);
+  excelJsonData.forEach(row => {
+    const key = `${row.Symbol}-${row.Side}-${row.Price}`;
+    if (!groupedRows[key]) {
+      groupedRows[key] = { ...row };
+    } else {
+      groupedRows[key].Quantity = (+groupedRows[key].Quantity + +row.Quantity).toFixed(8);
+      groupedRows[key].Amount = (+groupedRows[key].Amount + +row.Amount).toFixed(16);
+      groupedRows[key].Fee = (+groupedRows[key].Fee + +row.Fee).toFixed(8);
+      groupedRows[key]['Realized Profit'] = (+groupedRows[key]['Realized Profit'] + +row['Realized Profit']).toFixed(8);
     }
+  });
 
-    return mergedRows;
+  for (const key in groupedRows) {
+    mergedRows.push(groupedRows[key]);
+  }
+
+  return mergedRows;
 }
 
 
@@ -55,128 +55,129 @@ function handleMergeRows(excelJsonData) {
 
 export const buildTradesDataByBinanceCSV = async (ExcelFile, userId, accountId) => {
 
- 
-    const trades =[];
 
-    const workbook = XLSX.readFile(ExcelFile);
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
+  const trades = [];
 
-    // Convert the worksheet to a JSON object
-    const excelJsonData = XLSX.utils.sheet_to_json(worksheet);
-   
-    const mergedRows = handleMergeRows(excelJsonData);
-    for (let i = 0; i < mergedRows.length; i++) {
+  const workbook = XLSX.readFile(ExcelFile);
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
 
-        const trade = mergedRows[i];
+  // Convert the worksheet to a JSON object
+  const excelJsonData = XLSX.utils.sheet_to_json(worksheet);
 
-        const data = {
-            entryDate: trade["Date(UTC)"] || "",
-            symbol: trade["Symbol"] || "",
-            status: trade["Realized Profit"] < 0 ? "Loss" : trade["Realized Profit"] > 0 ? "Win" : "Break Even",
-            netROI: 0,
-            stopPrice: 0,
-            longShort: mergedRows['Side'] == "SELL" ? "Short" : "Long",
-            contracts: trade["Quantity"] || "",
-            entryPrice: trade["Price"] || "",
-            exitPrice: 0 ,
-            duration: "",
-            commission: parseFloat(trade["Fee"]).toFixed(2),
-            comments: "",
-            netPnL: trade["Realized Profit"],
-            transactionId:  uuidv4(), // Generate a unique ID using uuid
-        }
+  const mergedRows = handleMergeRows(excelJsonData);
+  for (let i = 0; i < mergedRows.length; i++) {
 
+    const trade = mergedRows[i];
 
-        //Code is repeat itself need to create functions for later use
-        const existingTrade = await Trade.findOne({ transactionId: data.transactionId });
-        console.log(existingTrade);
-        if (!existingTrade) {
-            const newTrade = await Trade.create({
-                entryDate: data.entryDate,
-                symbol: data.symbol,
-                status: data.status,
-                netROI: data.netROI,
-                longShort: data.longShort,
-                contracts: data.contracts,
-                entryPrice: data.entryPrice,
-                stopPrice: data.stopPrice,
-                exitPrice: data.exitPrice,
-                duration: data.duration,
-                commission:data.commission,
-                netPnL: data.netPnL,
-                transactionId: data.transactionId
-            });
-
-            trades.push(newTrade);
-            const user = await User.findById(userId);
-            const account = user.accounts.find(acc => acc._id == accountId);
-            account.trades.push(newTrade);
-            const tempAccounts = user.accounts.filter(acc => acc._id != accountId);
-            tempAccounts.push(account);
-            await User.updateOne({ _id: userId }, { accounts: tempAccounts });
-            await SelectedAccountModel.updateOne({ userId }, { accountId, account });
-
-        }
-    }
-    await Account.updateOne({ _id: accountId }, { trades: trades }); //pararm 1 = where update  param 2 = what update 
-
-
-    for (const data of excelJsonData) {
-        let trade = await Trade.findOne({ symbol: data.Symbol, longShort: data.Side == "SELL" ? "Short" : "Long" });
-   
-        if (trade) {
-            trade.tradesHistory.push({
-                entryDate: data["Date(UTC)"] || "",
-                symbol: data["Symbol"] || "",
-                status: data["Realized Profit"] < 0 ? "Loss" : data["Realized Profit"] > 0 ? "Win" : "Break Even",
-                netROI: 0,
-                stopPrice: 0,
-                longShort: data['Side'] == "SELL" ? "Short" : "Long",
-                contracts: data["Quantity"] || "",
-                entryPrice: data["Price"] || "",
-                exitPrice: 0 ,
-                duration: "",
-                commission: parseFloat(data["Fee"]).toFixed(2),
-                comments: "",
-                netPnL: data["Realized Profit"],
-                transactionId:  trade.transactionId, // Generate a unique ID using uuid
-    });
-    
-            await trade.save();
-        } else {
-            console.log(`No trade found for symbol ${data.Symbol} and longShort ${data.Side}`);
-        }
+    const data = {
+      entryDate: trade["Date(UTC)"] || "",
+      symbol: trade["Symbol"] || "",
+      status: trade["Realized Profit"] < 0 ? "Loss" : trade["Realized Profit"] > 0 ? "Win" : "Break Even",
+      netROI: 0,
+      stopPrice: 0,
+      longShort: mergedRows['Side'] == "SELL" ? "Short" : "Long",
+      contracts: trade["Quantity"] || "",
+      entryPrice: trade["Price"] || "",
+      exitPrice: 0,
+      duration: "",
+      commission: parseFloat(trade["Fee"]).toFixed(2),
+      comments: "",
+      netPnL: trade["Realized Profit"],
+      transactionId: uuidv4(), // Generate a unique ID using uuid
     }
 
 
+    //Code is repeat itself need to create functions for later use
+    const existingTrade = await Trade.findOne({ transactionId: data.transactionId });
+    console.log(existingTrade);
+    if (!existingTrade) {
+      const newTrade = await Trade.create({
+        entryDate: data.entryDate,
+        symbol: data.symbol,
+        status: data.status,
+        netROI: data.netROI,
+        longShort: data.longShort,
+        contracts: data.contracts,
+        entryPrice: data.entryPrice,
+        stopPrice: data.stopPrice,
+        exitPrice: data.exitPrice,
+        duration: data.duration,
+        commission: data.commission,
+        netPnL: data.netPnL,
+        transactionId: data.transactionId
+      });
+
+      trades.push(newTrade);
+      const user = await User.findById(userId);
+      const account = user.accounts.find(acc => acc._id == accountId);
+      account.trades.push(newTrade);
+      const tempAccounts = user.accounts.filter(acc => acc._id != accountId);
+      tempAccounts.push(account);
+      await User.updateOne({ _id: userId }, { accounts: tempAccounts });
+      await SelectedAccountModel.updateOne({ userId }, { accountId, account });
+
+    }
+  }
+  await Account.updateOne({ _id: accountId }, { trades: trades }); //pararm 1 = where update  param 2 = what update 
 
 
-    // excelJsonData.forEach(async data => {
-    
+  for (const data of excelJsonData) {
+    console.log('Before the object is been filtered-', { symbol: data.Symbol, longShort: data.Side == "SELL" ? "Short" : "Long" });
+    let trade = await Trade.findOne({ symbol: data.Symbol, longShort: data.Side == "SELL" ? "Short" : "Long" });
+    console.log(trade);
+    if (trade) {
+      trade.tradesHistory.push({
+        entryDate: data["Date(UTC)"] || "",
+        symbol: data["Symbol"] || "",
+        status: data["Realized Profit"] < 0 ? "Loss" : data["Realized Profit"] > 0 ? "Win" : "Break Even",
+        netROI: 0,
+        stopPrice: 0,
+        longShort: data['Side'] == "SELL" ? "Short" : "Long",
+        contracts: data["Quantity"] || "",
+        entryPrice: data["Price"] || "",
+        exitPrice: 0,
+        duration: "",
+        commission: parseFloat(data["Fee"]).toFixed(2),
+        comments: "",
+        netPnL: data["Realized Profit"],
+        transactionId: trade.transactionId, // Generate a unique ID using uuid
+      });
+
+      await trade.save();
+    } else {
+      console.log(`No trade found for symbol ${data.Symbol} and longShort ${data.Side}`);
+    }
+  }
 
 
-    //   let trade =  await Trade.findOne({symbol: data.Symbol, longShort: data.Side});
-    
-    //   trade.tradesHistory.push({
-    //     entryDate: data.entryDate,
-    //     symbol: data.symbol,
-    //     status: data.status,
-    //     netROI: data.netROI,
-    //     longShort: data.longShort,
-    //     contracts: data.contracts,
-    //     entryPrice: data.entryPrice,
-    //     stopPrice: data.stopPrice,
-    //     exitPrice: data.exitPrice,
-    //     duration: data.duration,
-    //     commission:data.commission,
-    //     netPnL: data.netPnL,
-    //     transactionId: data.transactionId
-    // });
-    // trade.save();
 
-    // });
-  
+
+  // excelJsonData.forEach(async data => {
+
+
+
+  //   let trade =  await Trade.findOne({symbol: data.Symbol, longShort: data.Side});
+
+  //   trade.tradesHistory.push({
+  //     entryDate: data.entryDate,
+  //     symbol: data.symbol,
+  //     status: data.status,
+  //     netROI: data.netROI,
+  //     longShort: data.longShort,
+  //     contracts: data.contracts,
+  //     entryPrice: data.entryPrice,
+  //     stopPrice: data.stopPrice,
+  //     exitPrice: data.exitPrice,
+  //     duration: data.duration,
+  //     commission:data.commission,
+  //     netPnL: data.netPnL,
+  //     transactionId: data.transactionId
+  // });
+  // trade.save();
+
+  // });
+
 
 };
 
