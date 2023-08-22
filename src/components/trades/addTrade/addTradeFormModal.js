bffeb49da151f4dea5f011338315d277e4be12f9
mynameismatan <matanamar165@gmail.com>
2023-08-22 20:40:18 +0300

    change add trade form

--- src/components/trades/addTrade/addTradeFormModal.js
+++ src/components/trades/addTrade/addTradeFormModal.js
@@ -27,7 +27,7 @@ import './addTrade.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectCurrentAccount, selectUser, setTradesList } from '../../../redux-toolkit/userSlice';
 import { configAuth } from '../../../api/configAuth';
-
+import {brokers} from '../../brokersNames/brokers.js'
 const style = {
   position: 'absolute',
   top: '50%',
@@ -198,7 +198,7 @@ export default function TradeModal(props) {
               else if (positionSymbol === "") notifyToast("Position symbol is missing", "warning");
               else if (!positionDate) notifyToast("Date field is missing", "warning");
               else if (entryPrice < 1) notifyToast(" entry Price is missing", "warning");
-              else if (exitPrice < 1) notifyToast("exit Price  is missing", "warning");
+              else if (exitPrice < 1 && currentAccount?.Broker === brokers.Tradovate) notifyToast("exit Price  is missing", "warning");
 
 
               return false;
@@ -235,7 +235,7 @@ export default function TradeModal(props) {
       else if (positionSymbol === "") notifyToast("Position symbol is missing", "warning");
       else if (!positionDate) notifyToast("Date field is missing", "warning");
       else if (entryPrice < 1) notifyToast(" entry Price is missing", "warning");
-      else if (exitPrice < 1) notifyToast("exit Price  is missing", "warning");
+      else if (exitPrice < 1 && currentAccount?.Broker === brokers.Tradovate)  notifyToast("exit Price  is missing", "warning");
 
 
 
@@ -290,6 +290,8 @@ export default function TradeModal(props) {
   }, [selectedFile])
 
   return (
+    
+    currentAccount?.Broker === brokers.Tradovate ?
     <Modal
       open={props.openModal}
       onClose={handleClose}
@@ -550,6 +552,227 @@ export default function TradeModal(props) {
           </Item >
         </Grid >
       </Box >
-    </Modal >
+    </Modal > :  currentAccount?.Broker === brokers.Binance ? <Modal
+      open={props.openModal}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={28}>
+
+
+            <Grid item xs={6} md={7}>
+              <Item>
+                {' '}
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Add New Trade
+                </Typography>
+              </Item>
+            </Grid>
+
+            <Grid item xs={6} md={4}>
+              <Grid item xs={6} md={4}>
+                <label htmlFor="file-input">
+                  <input ref={fileInputRef} name="file" type="file" accept="image/*" style={{ display: 'none' }} onChange={handleFileChange} />
+                  <Button
+                    size="small"
+                    variant="outlined"
+                    component="span"
+                    startIcon={<Iconify icon={'eva:file-add-outline'} />}
+                    onClick={handleButtonClick}
+                  >
+                    Upload Image
+                  </Button>
+                </label>
+
+              </Grid>
+            </Grid>
+          </Grid >
+        </Box >
+
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+          <Grid item xs={6}>
+            <Item>
+              <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' } }} noValidateautoComplete="off">
+                <TextField
+                  id="outlined-multiline-flexible"
+                  label="Comments"
+                  value={comments}
+                  multiline
+                  maxRows={7}
+                  onChange={(e) => handlePositionFieldInput(e, 'comments')}
+                />
+              </Box>{' '}
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>
+              <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' } }} noValidateautoComplete="off">
+                <TextField id="standard-basic" required="true"
+                  value={positionDate}
+                  onChange={(e) => handlePositionFieldInput(e, 'positionDate')}
+                  label="Open Date" variant="standard" type="date" />
+              </Box>{' '}
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>
+              <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-standard-label">Type</InputLabel>
+                <Select
+                  labelId="demo-simple-select-standard-label"
+                  id="demo-simple-select-standard"
+                  value={positionType}
+                  onChange={(e) => handlePositionFieldInput(e, 'positionType')}
+                  label="Type"
+                  required="true"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={'Short'}>Short</MenuItem>
+                  <MenuItem value={'Long'}>Long</MenuItem>
+
+                </Select>
+              </FormControl>
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>
+              <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' } }} noValidateautoComplete="off">
+                <Tabs value={value} onChange={handleChange} centered >
+                  <Tab label="Futuers" style={labelStyle} />
+                  <Tab label="Stocks" style={labelStyle} />
+                  <Tab label="Crypto" style={labelStyle} />
+                </Tabs>
+                <Autocomplete
+                  disablePortal
+                  id="combo-box-demo"
+                  required="true"
+                  options={activeTickers}
+                  value={positionSymbol}
+                  onChange={(e, newValue) => { handlePositionFieldInput(newValue, 'positionSymbol') }}
+                  sx={{ width: 600 }}
+                  renderInput={(params) => <TextField {...params} label="Symbol" value={positionSymbol} variant="standard" />}
+                />
+              </Box>
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+
+            <Item>
+              {' '}
+              <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-standard-label">Status</InputLabel>
+                <Select
+                  labelId="demo-simple-select-standard-label"
+                  id="demo-simple-select-standard"
+                  value={positionStatus}
+                  onChange={(e) => handlePositionFieldInput(e, 'positionStatus')}
+                  label="Status"
+                  required="true"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={'Win'}>Win</MenuItem>
+                  <MenuItem value={'Loss'}>Loss</MenuItem>
+                  <MenuItem value={'BreakEven'}>Break Even</MenuItem>
+                </Select>
+              </FormControl>
+            </Item>
+
+          </Grid>
+        </Grid>
+
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
+     
+          <Grid item xs={6}>
+            <Item>
+              {' '}
+              <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' } }} noValidateautoComplete="off">
+                <TextField
+                  className="outlined-number"
+
+                  type="number"
+                  value={positionCommision}
+                  onChange={(e) => handlePositionFieldInput(e, 'positionCommision')}
+                  onFocus={(e) => clearPositionFieldInput(e, 'positionCommision')}
+                  label="Commission"
+                  InputLabelProps={{ shrink: true }}
+                />
+              </Box>
+            </Item>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Item>
+              {' '}
+              <TextField
+                className="outlined-number"
+
+                label="Entry Price"
+                value={entryPrice}
+                type="number"
+                onChange={(e) => handlePositionFieldInput(e, 'entryPrice')}
+                onFocus={(e) => clearPositionFieldInput(e, 'entryPrice')}
+                InputLabelProps={{ shrink: true }}
+              />
+            </Item>
+          </Grid>
+         
+          <Grid item xs={6}>
+            <Item>
+              <TextField
+                className="outlined-number"
+                required="true"
+                label="Quantity"
+                value={contractsCounts}
+                onChange={(e) => handlePositionFieldInput(e, 'contractsCounts')}
+                onFocus={(e) => clearPositionFieldInput(e, 'contractsCounts')}
+                type="number"
+                InputLabelProps={{ shrink: true }}
+              />
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>
+              {' '}
+              <TextField
+                className="outlined-number"
+                required="true"
+                label="Net P&L"
+                value={netPnL}
+                onChange={(e) => handlePositionFieldInput(e, 'netPnL')}
+                onFocus={(e) => clearPositionFieldInput(e, 'netPnL')}
+                type="number"
+                InputLabelProps={{ shrink: true }}
+              />
+            </Item>
+          </Grid>
+
+         
+          <Grid item xs={6}>
+            <Item>
+              <TextField
+                className="outlined-number"
+                label="Stop loss"
+                type="number"
+                value={stopPrice}
+                onChange={(e) => handlePositionFieldInput(e, 'stopPrice')}
+                onFocus={(e) => clearPositionFieldInput(e, 'stopPrice')}
+                InputLabelProps={{ shrink: true }}
+              />
+            </Item>
+          </Grid>
+          <Item>
+            <br />
+            <Button variant="contained" onClick={handleSaveTrade}>Save</Button>
+          </Item >
+        </Grid >
+      </Box >
+    </Modal > : ""
   );
 }
