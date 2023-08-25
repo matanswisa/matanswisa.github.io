9c028edeb935e0a19b126cf1430f85ebee73f0d8
mynameismatan <matanamar165@gmail.com>
2023-08-25 13:45:33 +0300

    ..

--- utils/csvTradesFileUtils.js
+++ utils/csvTradesFileUtils.js
@@ -203,84 +203,84 @@ export const buildTradesDataByTradovateCSV = async (csvData, userId, accountId) 
         console.log("fffffffffffffffffffff" , father);
         // const mainTrades = subTrades[i];
 
-        for (let j = 0; j < mainTrades.length;j++){
-        const trade = mainTrades[i];
+    //     for (let j = 0; j < mainTrades.length;j++){
+    //     const trade = mainTrades[i];
 
-        let commissionSize = calcCommission(trade["Product Description"]) * -1;
-        let totalCommissionInDollars = commissionSize * trade["Bought"];
+    //     let commissionSize = calcCommission(trade["Product Description"]) * -1;
+    //     let totalCommissionInDollars = commissionSize * trade["Bought"];
 
 
-        //this code responsible to build a main trade partials we add after - 
+    //     //this code responsible to build a main trade partials we add after - 
 
-        const netROI = ((trade["Sell Price"] - trade["Buy Price"]) / trade["Buy Price"]) * 100;
-        const data = {
-            entryDate: trade["Bought Timestamp"] || "",
-            symbol: trade["Product"] || "",
-            status: trade["P/L"] < 0 ? "Loss" : trade["P/L"] > 0 ? "Win" : "Break Even",
-            netROI: netROI.toFixed(2),
-            stopPrice: 0,
+    //     const netROI = ((trade["Sell Price"] - trade["Buy Price"]) / trade["Buy Price"]) * 100;
+    //     const data = {
+    //         entryDate: trade["Bought Timestamp"] || "",
+    //         symbol: trade["Product"] || "",
+    //         status: trade["P/L"] < 0 ? "Loss" : trade["P/L"] > 0 ? "Win" : "Break Even",
+    //         netROI: netROI.toFixed(2),
+    //         stopPrice: 0,
           
-            longShort: mergedRows[i]['LongShort'],
-            contracts: trade["Bought"] || "",
-            entryPrice: trade["Buy Price"] || "",
-            exitPrice: trade["Sell Price"] || "",
-            duration: "",
-            commission: totalCommissionInDollars,
-            comments: "",
-            netPnL: trade["P/L"] !== undefined ? trade["P/L"] + totalCommissionInDollars : "",
-            transactionId: trade["Position ID"],
-        }
+    //         longShort: mergedRows[i]['LongShort'],
+    //         contracts: trade["Bought"] || "",
+    //         entryPrice: trade["Buy Price"] || "",
+    //         exitPrice: trade["Sell Price"] || "",
+    //         duration: "",
+    //         commission: totalCommissionInDollars,
+    //         comments: "",
+    //         netPnL: trade["P/L"] !== undefined ? trade["P/L"] + totalCommissionInDollars : "",
+    //         transactionId: trade["Position ID"],
+    //     }
 
 
 
 
-        const boughtTimestamp = new Date(trade["Bought Timestamp"]);
-        const soldTimestamp = new Date(trade["Sold Timestamp"]);
+    //     const boughtTimestamp = new Date(trade["Bought Timestamp"]);
+    //     const soldTimestamp = new Date(trade["Sold Timestamp"]);
 
 
-        const timeDifferenceInMinutes = (soldTimestamp - boughtTimestamp) / (1000 * 60);
-        const absoluteDurationInMinutes = Math.abs(timeDifferenceInMinutes)
-        data.duration = absoluteDurationInMinutes || "";
+    //     const timeDifferenceInMinutes = (soldTimestamp - boughtTimestamp) / (1000 * 60);
+    //     const absoluteDurationInMinutes = Math.abs(timeDifferenceInMinutes)
+    //     data.duration = absoluteDurationInMinutes || "";
 
 
 
-        if (data.status == "Break Even") {
+    //     if (data.status == "Break Even") {
 
-            const timeDifferenceInMinutes = (soldTimestamp - boughtTimestamp) / (1000 * 60);
-            data.duration = timeDifferenceInMinutes || "";
-            data.netROI = 0;
-        }
+    //         const timeDifferenceInMinutes = (soldTimestamp - boughtTimestamp) / (1000 * 60);
+    //         data.duration = timeDifferenceInMinutes || "";
+    //         data.netROI = 0;
+    //     }
 
-        //Code is repeat itself need to create functions for later use
-        const existingTrade = await Trade.findOne({ transactionId: data.transactionId });
+    //     //Code is repeat itself need to create functions for later use
+    //     const existingTrade = await Trade.findOne({ transactionId: data.transactionId });
 
-        if (!existingTrade) {
-            const newTrade = await Trade.create({
-                entryDate: data.entryDate,
-                symbol: data.symbol,
-                status: data.status,
-                netROI: data.netROI,
-                longShort: data.longShort,
-                contracts: data.contracts,
-                entryPrice: data.entryPrice,
-                stopPrice: data.stopPrice,
-                exitPrice: data.exitPrice,
-                duration: data.duration,
-                commission: data.commission,
-                netPnL: data.netPnL,
-                transactionId: data.transactionId
-            });
+    //     if (!existingTrade) {
+    //         const newTrade = await Trade.create({
+    //             entryDate: data.entryDate,
+    //             symbol: data.symbol,
+    //             status: data.status,
+    //             netROI: data.netROI,
+    //             longShort: data.longShort,
+    //             contracts: data.contracts,
+    //             entryPrice: data.entryPrice,
+    //             stopPrice: data.stopPrice,
+    //             exitPrice: data.exitPrice,
+    //             duration: data.duration,
+    //             commission: data.commission,
+    //             netPnL: data.netPnL,
+    //             transactionId: data.transactionId
+    //         });
 
 
-            const user = await User.findById(userId);
-            const account = user.accounts.find(acc => acc._id == accountId);
-            account.trades.push(newTrade);
-            const tempAccounts = user.accounts.filter(acc => acc._id != accountId);
-            tempAccounts.push(account);
-            await User.updateOne({ _id: userId }, { accounts: tempAccounts });
-            await SelectedAccountModel.updateOne({ userId }, { accountId, account });
-        }
-    }
+    //         const user = await User.findById(userId);
+    //         const account = user.accounts.find(acc => acc._id == accountId);
+    //         account.trades.push(newTrade);
+    //         const tempAccounts = user.accounts.filter(acc => acc._id != accountId);
+    //         tempAccounts.push(account);
+    //         await User.updateOne({ _id: userId }, { accounts: tempAccounts });
+    //         await SelectedAccountModel.updateOne({ userId }, { accountId, account });
+    //     }
+    // }
 
     }
 }
