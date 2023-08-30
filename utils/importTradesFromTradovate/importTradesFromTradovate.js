fccb601bf7c4f817742072369977a903cf50eead
matan swisa <matanswisa01@gmail.com>
2023-08-30 15:41:11 +0300

    Change importTradesFromTradovate iteraion to while , modify DataRemoveByIdx

--- utils/importTradesFromTradovate/importTradesFromTradovate.js
+++ utils/importTradesFromTradovate/importTradesFromTradovate.js
@@ -5,36 +5,43 @@ export const importTradesFromTradovate = async (csvData, userId, accountId) => {
     trades = removeLeadingSpaces(trades);
     const filteredTrades = filterCanceledTrades(trades);
     let sortedTrades = sortByTimestamp(filteredTrades);
-  
-    for(let i = 0 ;i < 15; i++){  /// runing on intire data
-
-        let EndIdxOfCurrTrade = (CalcIndexsOfTradeByCsvData(sortedTrades));   //get index of end in curr trade.
-     
-        calcTradeDataAndInsertToDb(sortedTrades,i,EndIdxOfCurrTrade);   // handle trade info and insert to db.
-        sortedTrades = DataRemovalByIdx(sortedTrades, CalcIndexsOfTradeByCsvData(sortedTrades)); // after  insert db remove from orginal data  the curr trade and going to next trade.
-       
+    let i = 0;
+    let EndIdxOfCurrTrade = 0;
+    while (sortedTrades.length != 0) {  /// runing on intire data
+        EndIdxOfCurrTrade = (CalcIndexsOfTradeByCsvData(sortedTrades));   //get index of end in curr trade.
+        if (sortedTrades.length == (EndIdxOfCurrTrade + 1)) { //in case the reminder left we save it inside the db and setting the value of sortedTrades to empty array.
+            calcTradeDataAndInsertToDb(sortedTrades, i, EndIdxOfCurrTrade);   // handle trade info and insert to db.
+            sortedTrades = []
+        } else {
+            calcTradeDataAndInsertToDb(sortedTrades, i, EndIdxOfCurrTrade);   // handle trade info and insert to db.
+            sortedTrades = DataRemovalByIdx(sortedTrades, EndIdxOfCurrTrade); // after  insert db remove from orginal data  the curr trade and going to next trade.
+        }
+        i++;
     }
 
-  //  printTrades(sortedTrades);
+    calcTradeDataAndInsertToDb(sortedTrades, i, EndIdxOfCurrTrade + 1);   // handle trade info and insert to db.
+
+
+    //  printTrades(sortedTrades);
 }
 
 function removeLeadingSpaces(trades) {
     for (const trade of trades) {
-      for (const field in trade) {
-        if (typeof trade[field] === 'string') {
-          trade[field] = trade[field].trim();
+        for (const field in trade) {
+            if (typeof trade[field] === 'string') {
+                trade[field] = trade[field].trim();
+            }
         }
-      }
     }
     return trades;
-  }
+}
 
 
 const filterCanceledTrades = (trades) => {
-    for(let i = 0 ;i < trades.length;i++){
+    for (let i = 0; i < trades.length; i++) {
         console.log(trades[0]["Status"]);
     }
-    return trades.filter(trade => trade.Status !== ' Canceled') ;
+    return trades.filter(trade => trade.Status !== 'Canceled');
 
 }
 
@@ -63,12 +70,12 @@ const sortByTimestamp = (trades) => {
 //         buySideSymbol = 1; // Update values based on trade type
 //         sellSideSymbol = -1; // Update values based on trade type
 //     }
-    
+
 //     for (let row = 0; row < trades.length; row++) {
-    
+
 //         const filledQty = parseInt(trades[row]["filledQty"], 10);
 //         const tradeSideSymbol = trades[row]["B/S"] == ' Sell' || trades[row]["B/S"] == 'Sell' ? sellSideSymbol : buySideSymbol;
-        
+
 //         // console.log(`Row ${row}: tradeSideSymbol = ${tradeSideSymbol}`); // Debug print
 //         // console.log("tradesign",tradeSideSymbol);
 //         diffBettwenConract += filledQty * tradeSideSymbol;
@@ -79,7 +86,7 @@ const sortByTimestamp = (trades) => {
 //         }
 //     }
 
-   
+
 //     return endOfTradeRowIdx;
 // }
 
@@ -90,15 +97,15 @@ const CalcIndexsOfTradeByCsvData = (trades) => {
     let diffBettwenSell = 0;
 
     for (let row = 0; row < trades.length; row++) {
-   
+
         const filledQty = parseInt(trades[row]["filledQty"], 10);
-      
+
         if (trades[row]["B/S"] == ' Sell' || trades[row]["B/S"] == 'Sell' || trades[row]["B/S"] == '  Sell') {
             diffBettwenSell += filledQty;
-      
+
         } else if (trades[row]["B/S"] == " Buy" || trades[row]["B/S"] == "Buy" || trades[row]["B/S"] == "  Buy") {
             diffBettwenBuy += filledQty;
-           
+
         }
 
         if (diffBettwenBuy === diffBettwenSell) {
@@ -111,15 +118,18 @@ const CalcIndexsOfTradeByCsvData = (trades) => {
 }
 
 
-const DataRemovalByIdx = (data,EndIdxOfCurrTrade ) => 
-{
-    const newData = data.slice(EndIdxOfCurrTrade + 1);
-    return newData; 
+const DataRemovalByIdx = (data, EndIdxOfCurrTrade) => {
+    let newData = (data.length == EndIdxOfCurrTrade + 1) ? data.slice(EndIdxOfCurrTrade - 1, EndIdxOfCurrTrade + 1) : data.slice(EndIdxOfCurrTrade + 1);
+    // if (data.length == EndIdxOfCurrTrade + 1) {
+    //     data = data.slice(EndIdxOfCurrTrade + 1);
+
+    // }
+    console.log("You got sliced champ , current data-", data.length, "After I cut you big boy-", newData.length);
+    return newData;
 }
 
 
-const calcTradeDataAndInsertToDb = (data,i,EndIdxOfCurrTrade) =>
-{
+const calcTradeDataAndInsertToDb = (data, i, EndIdxOfCurrTrade) => {
 
     // let data = {
     //     netPNL: 0,
@@ -132,12 +142,12 @@ const calcTradeDataAndInsertToDb = (data,i,EndIdxOfCurrTrade) =>
     //   const sellTrade = trades[1];
 
     //   for(let trade = 0 ; trade <= EndIdxOfCurrTrade; trade++){
-          
+
     //       console.log(data[trade]);
-          
+
     //     }
-        
-     //   console.log("trade number ",i+1 ," indexes  is from  0 to :" ,  EndIdxOfCurrTrade);
+
+    //   console.log("trade number ",i+1 ," indexes  is from  0 to :" ,  EndIdxOfCurrTrade);
 
 }
 
