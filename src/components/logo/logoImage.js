bae21d759599594be673e953618401f505ad7efd
mynameismatan <matanamar165@gmail.com>
2023-09-10 20:33:27 +0300

    ..

--- src/components/logo/logoImage.js
+++ src/components/logo/logoImage.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import imageSrc from './tradeExalt.png'; // Replace with the actual path to your image
+import imageSrc from './tradeExalt.png';
+import imageSrcWhite from './tradeExaltWhite.png';
+import { selectDarkMode } from '../../redux-toolkit/darkModeSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const MyComponentWithImage = (props) => {
   const { w, h } = props;
-  return (
+  const darkMode = useSelector(selectDarkMode);
+    return (
     <div>
       
       {/* <img  style={{ width: '240px', height: '170px' }}  src={imageSrc} alt="Description of the image" /> */}
-      <img  style={{ width: w, height: h }}  src={imageSrc} alt="Description of the image" />
+      <img  style={{ width: w, height: h }}  src={ darkMode === false ? imageSrc : imageSrcWhite} alt="Description of the image" />
      
     </div>
   );
