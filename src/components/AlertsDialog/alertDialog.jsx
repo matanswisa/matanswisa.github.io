1f5c37982959ae7f5aa8843ae177a1a87c8d6b4d
mynameismatan <matanamar165@gmail.com>
2023-10-06 22:41:18 +0300

    alerts_update

--- src/components/AlertsDialog/alertDialog.jsx
+++ src/components/AlertsDialog/alertDialog.jsx
@@ -2,28 +2,35 @@ import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, S
 import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
 
-import { selectAlerts } from '../../redux-toolkit/userSlice';
-
+import { selectAlerts, selectUser, setAlerts } from '../../redux-toolkit/userSlice';
+import Divider from '@mui/material/Divider';
+import MuiAlert from '@mui/material/Alert';
+import api from '../../api/api';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
 
-export default function AlertDialog(props) {
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
 
-    const alerts = useSelector(selectAlerts);
+export default function AlertDialog(props) {
    
+    const alerts = useSelector(selectAlerts);
+    const user = useSelector(selectUser);
     const { alert, key } = props;
     const [open, setOpen] = React.useState(true);
-
+    const reduxDispatch = useDispatch()
     const AlertsMessages = [
         {
+            Title:"Over trading",
             ContentText: "you exceed your limit trades per day ",
             msgText: "Number of trades for today:",
             limit: "Your limit trades :"
         },
-        {
+        {   Title:"Losses in a row",
             ContentText: "you lose a certain number of times in a row for today.",
             msgText: "Number of lose for today:",
             limit: "Your lose limit trades:"
@@ -32,14 +39,42 @@ export default function AlertDialog(props) {
 
 
 
-    const handleClose = () => {
+    const handleClose = (index) => {
+        turnOffAlert(index);
         setOpen(false);
     }
 
 
 
+
+  const turnOffAlert = async (index) => {
+    const data = {
+      userId: user._id,
+      indexofAlert: index,
+    };
+
+    try {
+      const response = await api.put('/api/auth/TurnAlertOff', data, {
+        headers: { Authorization: "Bearer " + user.accessToken }
+      });
+
+      if (response.status === 200) {
+      
+        reduxDispatch(setAlerts(response.data));
+        // You can also do other actions here if needed
+      } else {
+        // Handle other status codes, e.g., 400, 500, etc.
+        console.log("Request failed with status code:", response.status);
+      }
+    } catch (err) {
+      // Handle any exceptions that occurred during the request
+      console.error(err);
+      // Handle the error as needed
+    }
+  };
+    
     return (
-        <Dialog
+        <Dialog 
             key={key}
             open={open}
             TransitionComponent={Transition}
@@ -47,15 +82,16 @@ export default function AlertDialog(props) {
             onClose={handleClose}
             aria-describedby="alert-dialog-slide-description"
         >
-            <DialogTitle>Alert</DialogTitle>
+            <DialogTitle>  <Alert severity="error"  style={{fontSize:'20px',fontFamily:'unset' ,width: '100%',}}> Alert - <span style={{fontSize:'20px', fontFamily:'unset'}}>{AlertsMessages[alert.alertNumber].Title}</span></Alert> </DialogTitle>
+            <Divider variant="middle" />
             <DialogContent>
-                <DialogContentText id="alert-dialog-slide-description" style={{ fontFamily: 'unset', fontWeight: 'bold', fontSize: '22px', color: 'red' }}>
+                <DialogContentText id="alert-dialog-slide-description" style={{ fontFamily: 'unset',  fontSize: '18px', color: 'red' }}>
                     {AlertsMessages[alert.alertNumber].ContentText}
                 </DialogContentText>
 
                 <Typography id="number" style={{ marginTop: '25px' }}>
                     {AlertsMessages[alert.alertNumber].msgText}
-                    <span style={{ display: 'inline-flex', alignItems: 'center', marginLeft: '5px' }}>
+                    <span style={{ display: 'inline-flex', alignItems: 'center', marginLeft: '5px' ,fontSize: '15px',  }}>
                         <h5 style={{ margin: '0' }}></h5>
                     </span>
                 </Typography>
@@ -68,7 +104,8 @@ export default function AlertDialog(props) {
                 </Typography>
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose}>Close</Button>
+            <Button onClick={() => handleClose(alert.alertNumber)}>Close</Button>
+
             </DialogActions>
         </Dialog>
     );
